2e01bb92c78672691dac3120b9b107b4ae69d737
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-08-14 14:01:44 +0530

    Handle news api using axios.

---
+++ front-end/src/services/cryptoNews.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+const key = import.meta.env.VITE_API_KEY;
+const host = import.meta.env.VITE_API_NEWS_HOST;
+
+const options = {
+  method: "GET",
+  url: `https://${host}/news/search?q=cryptocurrency`,
+  params: {
+    safeSearch: "Off",
+    textFormat: "Raw",
+  },
+  headers: {
+    "X-BingApis-SDK": "true",
+    "X-RapidAPI-Key": key,
+    "X-RapidAPI-Host": host,
+  },
+};
+
+const getNews = async () => {
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default getNews;
