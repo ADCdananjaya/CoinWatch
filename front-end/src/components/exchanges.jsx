2c9c0519badc177d53d8bc973b434825611d848e
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-08-15 22:44:08 +0530

    Restructure exchange component.

--- front-end/src/components/exchanges.jsx
+++ front-end/src/components/exchanges.jsx
@@ -1,20 +1,4 @@
-import { useState, useEffect } from "react";
-import { getExchanges } from "../services/cryptoApi";
-import Loading from "./loading";
-
 const Exchanges = () => {
-  const [exchanges, setExchanges] = useState();
-  console.log(exchanges);
-
-  useEffect(() => {
-    const getData = async () => {
-      const data = await getExchanges();
-      setExchanges(data);
-    };
-    getData();
-  }, []);
-
-  if (!exchanges) return <Loading />;
   return (
     <div className="w-full h-auto bg-slate-50 flex flex-col">
       <p className="text-2xl">Exchanges</p>
