a168b0e4445420075afae4f55beda80db0d7a382
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-08-16 13:37:30 +0530

    Add auth route for login users.

--- server/index.js
+++ server/index.js
@@ -5,11 +5,13 @@ require("./db/mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
 const users = require("./routes/users");
+const auth = require("./routes/auth");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 app.listen(port, () => console.log(`Appp listening on port ${port}!`));
