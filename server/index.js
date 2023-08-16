1c44cbf97ea14bc9934a0a1314f4ff4f6428eea0
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-08-16 13:31:26 +0530

    Add users router with user model.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 require("./db/mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
+const users = require("./routes/users");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/api/users", users);
+
 app.listen(port, () => console.log(`Appp listening on port ${port}!`));
